7afee183adeebbe44f6ad40ae7056c7b92f10a80
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-10-18 01:17:50 +0530

    Added some cool features through JS...

---
+++ script.js
@@ -0,0 +1,43 @@
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+const progress = document.getElementById("progress");
+
+let activeCircle = 1;
+
+next.addEventListener("click", () => {
+  activeCircle++;
+  if (activeCircle > circles.length) {
+    activeCircle = circles.length;
+  }
+  update();
+});
+prev.addEventListener("click", () => {
+  activeCircle--;
+  if (activeCircle < 1) {
+    activeCircle = 1;
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < activeCircle) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (activeCircle === 1) {
+    prev.disabled = true;
+  } else if (activeCircle === circles.length) {
+    next.disabled = true;
+  } else {
+    next.disabled = false;
+    prev.disabled = false;
+  }
+}
